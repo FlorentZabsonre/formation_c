5178c7523115ab3b6f41ec461a191e709c495469
Aokijikuzan <florenzabs@hotmail.fr>
2020-08-01 17:07:59 +0200

    debut de la revison sur le langage c

---
+++ Hello_world/main.c
@@ -0,0 +1,10 @@
+// stdio permet d'afficher du texte a l'ecran
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Salut  !\n");
+    printf(" \t Apprendre a coder en \"C \"   ! \n");
+    return 0;
+}
