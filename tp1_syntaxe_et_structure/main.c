484c04bb2b284afb3c1b0c8cb6c21456f2778b5b
Aokijikuzan <florenzabs@hotmail.fr>
2020-08-01 18:12:24 +0200

    suite

--- tp1_syntaxe_et_structure/main.c
+++ tp1_syntaxe_et_structure/main.c
@@ -1,8 +1,10 @@
+//stdio permet de faire apparaitre du texte a l'ecran
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+ //main est la fonction principale de ce projet et retourne un entier,ici 0,
+ int main()
+ {
+     printf("TP 1, \"Syntaxe et structure\" ");
+    printf(  "\n \t Hello World!");
+      return 0;
 }
